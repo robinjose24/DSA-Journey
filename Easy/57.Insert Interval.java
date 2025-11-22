5562580c1d56f28e9a9340211eadbe0998e6afa7
ROBIN JOSE <160265144+robinjose24@users.noreply.github.com>
2025-11-22 13:45:38 +0530

    Implement insert interval function

---
+++ Easy/57.Insert Interval.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
+        int i=0;
+
+        ArrayList<int[]> list=new ArrayList<>();
+        //add left NON-OVERLAPPING SECTION
+        while(i<n && intervals[i][1]<newInterval[0]){
+            list.add(intervals[i]);
+            i++;
+        }
+        //add middle OVERLAPPING SECTION
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        list.add(newInterval);
+
+        //add right NON-OVERLAPPING SECTION
+        while(i<n){
+            list.add(intervals[i]);
+            i++;
+        }
+        return list.toArray(new int[0][]);
+    }
+}
